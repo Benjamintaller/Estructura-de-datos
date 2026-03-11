db55efb08a5fdcacdb8e0d898f13e252a09f0010
Benjamintaller <a2025113757@estudiantes.upsa.edu.bo>
2026-03-11 13:32:29 -0400

    Create Triangulo.h

---
+++ Triangulo.h
@@ -0,0 +1,11 @@
+#include "Triangulo.h"
+//Declaracion de metodos o funciones 
+
+Triangulo::Triangulo() { //constructor
+}
+Triangulo::~Triangulo(){
+}
+float Triangulo::calculararea(float b, float h) {
+	return (b * h) / 2;
+}
+
